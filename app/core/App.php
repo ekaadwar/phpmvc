e399d0303f10b81faeb626b872cffc15ba3d94e8
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-30 06:23:15 +0700

    menambhakan komentar

--- app/core/App.php
+++ app/core/App.php
@@ -3,14 +3,14 @@
 class App{
 	public function __construct(){
 		$url = $this->parseURL();
-		var_dump($url);
+		var_dump($url);//mengambil niali url
 	}
 
 	public function parseURL(){
 		if( isset($_GET['url']) ){
-			$url = rtrim($_GET['url'],'/');
-			$url = filter_var($url, FILTER_SANITIZE_URL);
-			$url = explode('/', $url);
+			$url = rtrim($_GET['url'],'/'); //menghilangkan tanda '/' di akhir url
+			$url = filter_var($url, FILTER_SANITIZE_URL); //menfilter karaker2 aneh
+			$url = explode('/', $url); //menjadikan tiap elemen url menjadi array
 			return $url;
 		}
 	}
