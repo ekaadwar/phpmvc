32e6310feca2543176c7da0ca340c63c9d2171bb
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-30 16:40:06 +0700

    selesai routing, masuk controller

--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,34 @@
 <?php 
-
 class App{
+	protected 	$controller = "Home", 
+				$method = "index", 
+				$params = [];
+
 	public function __construct(){
 		$url = $this->parseURL();
-		var_dump($url);//mengambil niali url
+		
+		// controller
+		if( file_exists('../app/controllers/' . $url[0]. '.php')  ){
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
+
+		require_once '../app/controllers/' . $this->controller . '.php';
+
+		$this->controller = new $this->controller;
+
+		// method
+		if( isset($url[1]) ){
+			if( method_exists($this->controller, $url[1]) ){
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		//parameter
+		if( !empty($url) ){
+			var_dump($url);
+		}
 	}
 
 	public function parseURL(){
