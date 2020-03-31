6545f44cf61a30136c3231ba957fa4e394e47cd9
Eka Fajhari Adwar <eka.adwar@gmail.com>
2020-03-31 14:27:38 +0700

    assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,6 @@
-	<h1>About Me</h1>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?=BASEURL; ?>/img/ekafajhariadwar.jpg" alt="Eka Fajhari Adwar" width="200" class="rounded-circle shadow">
 	<p>Halo, nama saya <?= $data['nama']; ?>. Umur saya <?= $data['umur']; ?> tahun. Saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
+	
